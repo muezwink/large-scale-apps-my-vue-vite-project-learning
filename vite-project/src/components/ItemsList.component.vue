4119b8a1ccd60d90c07650c038a8248006b42e0e
muezwink <muezwink@gmail.com>
2024-04-19 16:12:08 +0900

    Chapter 3 - Data Model Interfaces

--- vite-project/src/components/ItemsList.component.vue
+++ vite-project/src/components/ItemsList.component.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+  // import a reference to our ItemInterace
+  // ItemInterface에 대한 참조를 가져옵니다.
+  import type { ItemInterface } from '../../models/items/Item.interface'
   // expose a property called items with a default value of a blank array
-  defineProps<{ items: any[] }>()
-  // explicetely using any[] as we'll replace this with an interface in the next chapters 
+  // 기본값이 빈 배열인 항목이라는 프로퍼티를 노출합니다.
+  // here replace any[] with ItemInterace[] 
+  // 여기서 any[]를 ItemInterface[]로 대체합니다.
+  defineProps<{ items: ItemInterface[] }>() 
 </script>
 
 <template> 
